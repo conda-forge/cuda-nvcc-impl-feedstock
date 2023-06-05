e965c319937064a67e4072366a395ee084c96fe2
Daniel Ching <carterbox@users.noreply.github.com>
2023-06-05 14:16:11 -0500

    BUG: Install activate script

--- recipe/build.sh
+++ recipe/build.sh
@@ -39,10 +39,8 @@ done
 
 # Copy the [de]activate scripts to $PREFIX/etc/conda/[de]activate.d.
 # This will allow them to be run on environment activation.
-# Name this script starting with `~` so it is run after all other compiler activation scripts.
-# At the point of running this, $CXX must be defined.
-# for CHANGE in "activate" "deactivate"
-# do
-#     mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
-#     cp "${RECIPE_DIR}/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/~cuda-nvcc_${CHANGE}.sh"
-# done
+for CHANGE in "activate" "deactivate"
+do
+    mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
+    cp "${RECIPE_DIR}/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/cuda-nvcc-tools_${CHANGE}.sh"
+done
