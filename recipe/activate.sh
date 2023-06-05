40763a198f023dfec636d2f04f63e2f1ff09d373
Daniel Ching <carterbox@users.noreply.github.com>
2023-06-05 14:48:26 -0500

    Rename environment variable to avoid CMake collision

--- recipe/activate.sh
+++ recipe/activate.sh
@@ -6,20 +6,13 @@ set -ex
 [[ "@cross_target_platform@" == "linux-ppc64le" ]] && targetsDir="targets/ppc64le-linux"
 [[ "@cross_target_platform@" == "linux-aarch64" ]] && targetsDir="targets/sbsa-linux"
 
-CUDA_INCLUDE_DIRS=""
+CUDA_INCL_DIRS=""
 CUDA_LINK_DIRS=""
 if [ "${CONDA_BUILD:-0}" = "1" ]; then
-    CUDA_INCLUDE_DIRS="${CUDA_INCLUDE_DIRS} -I${PREFIX}/${targetsDir}/include"
-    CUDA_INCLUDE_DIRS="${CUDA_INCLUDE_DIRS} -I${BUILD_PREFIX}/${targetsDir}/include"
+    CUDA_INCL_DIRS="${CUDA_INCL_DIRS} -I${PREFIX}/${targetsDir}/include"
     CUDA_LINK_DIRS="${CUDA_LINK_DIRS} -L${PREFIX}/${targetsDir}/lib"
     CUDA_LINK_DIRS="${CUDA_LINK_DIRS} -L${PREFIX}/${targetsDir}/lib/stubs"
-    CUDA_LINK_DIRS="${CUDA_LINK_DIRS} -L${BUILD_PREFIX}/${targetsDir}/lib"
-    CUDA_LINK_DIRS="${CUDA_LINK_DIRS} -L${BUILD_PREFIX}/${targetsDir}/lib/stubs"
-else
-    CUDA_INCLUDE_DIRS="${CUDA_INCLUDE_DIRS} -I${CONDA_PREFIX}/${targetsDir}/include"
-    CUDA_LINK_DIRS="${CUDA_LINK_DIRS} -L${CONDA_PREFIX}/${targetsDir}/lib"
-    CUDA_LINK_DIRS="${CUDA_LINK_DIRS} -L${CONDA_PREFIX}/${targetsDir}/lib/stubs"
 fi
 
-export CUDA_INCLUDE_DIRS
+export CUDA_INCL_DIRS
 export CUDA_LINK_DIRS
